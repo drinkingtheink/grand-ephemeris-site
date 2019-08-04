8a0ca014dd3c560f9845eab2b3efca6153d04351
Jason M Harrison <jason.m.harrison@gmail.com>
2019-08-04 10:01:32 -0500

    Layout polish.

--- src/containers/MainStage.js
+++ src/containers/MainStage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GELogo from '../components/GELogo';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 class MainStage extends Component {
   constructor(props) {
@@ -27,29 +28,40 @@ class MainStage extends Component {
         <section className="main-panel">
           <h2>Discography</h2>
           <div className="discog-menu">
-            <ul>
-              <li><a href="#atlassian-breaks">Atlassian Breaks</a></li>
-              <li><a href="#votive-viscera">Votive Viscera</a></li>
-            </ul>
+            <AnchorLink href='#atlassian-breaks'>Atlassian Breaks</AnchorLink>
+            <AnchorLink href='#votive-viscera'>Votive Viscera</AnchorLink>
           </div>
 
-          <article className="album atlassian-breaks"> 
-            <a name="#atlassian-breaks"></a>
-            <h3>Atlassian Breaks - Released August, 23rd 2019</h3>
-            <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/817825176&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>  
+          <article className="album atlassian-breaks" id="atlassian-breaks"> 
+            <h3>Atlassian Breaks</h3>
+            
+            <section className="album-info">
+              <span>Released August, 23rd 2019</span>
+            </section>
+
+            <iframe title="Atlassian Breaks" width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/817825176&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>  
 
             <section className="lyrics">
               <h4>Lyrics:</h4>
             </section>        
           </article>
 
-          <article className="album votive-viscera">
-            <a name="#votive-viscera"></a>
-            <h3>Votive Viscera - Released August, 1st 2014</h3>
-            <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/45125373&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>            
+          <article className="album votive-viscera" id="votive-viscera">
+            <h3>Votive Viscera</h3>
+            
+            <section className="album-info">
+              <span>Released August, 1st 2014</span>
+            </section>
+
+            <iframe title="Votive Viscera" width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/45125373&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>            
             <section className="lyrics">
               <h4>Lyrics:</h4>
             </section>    
+          </article>
+
+          <article className="album covers">
+            <h3>Misc Covers</h3>
+            <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/821052595&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
           </article>
         </section>
       </main>
