6024edfabdffcddabcfbb447d53db9d1891f2414
Jason M Harrison <jason.m.harrison@gmail.com>
2019-08-28 15:20:15 -0500

    Lyric typo.

--- src/data/nunca-jamas.js
+++ src/data/nunca-jamas.js
@@ -1,5 +1,5 @@
 /* eslint no-useless-escape: 0 */
 
-const nj = "You broke all your favorite bones\nWhen I told you not to go\nBut you did anyway\nThey sent in the ransome note\nClipped words to clear their throats\nEnjoy your stay\nBut if they don\'nt make it right\nI\'ll snuff out their greedy side\nShow your face\nTo alleviate the pressure\nThey\'re going to melt the ice and show\nI heard a crack this morning\nThe tide\'s at my back door"
+const nj = "You broke all your favorite bones\nWhen I told you not to go\nBut you did anyway\nThey sent in the ransome note\nClipped words to clear their throats\nEnjoy your stay\nBut if they don\'t make it right\nI\'ll snuff out their greedy side\nShow your face\nTo alleviate the pressure\nThey\'re going to melt the ice and show\nI heard a crack this morning\nThe tide\'s at my back door"
 
 export default nj;
