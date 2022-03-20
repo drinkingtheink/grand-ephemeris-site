5fa01bae613e4a1b536094cf484ca5e2a5a7bfd0
drinkingtheink <jason.m.harrison@gmail.com>
2022-03-20 14:49:25 -0500

    tumult lyrics

--- src/data/tumult-etc.js
+++ src/data/tumult-etc.js
@@ -1,5 +1,5 @@
 /* eslint no-useless-escape: 0 */
 
-const te = "\nAudio taken from Caltech - Richard Feynman lecture on 'Atoms in motion' Sept. 26th, 1961: www.feynmanlectures.caltech.edu/flptapes.html\n'But each piece, or part of the whole of nature is only some kind of an approximation to the complete truth...'\n'Even everything that we know is only some kind of approximation...'\n'But what is the source of the knowledge, where do the laws come from that are going to be tested?'\n'...to guess at the wonderful, simple, yet very strange patterns beneath it all...'";
+const te = "\nAudio taken from Caltech - Richard Feynman lecture on 'Atoms in motion' Sept. 26th, 1961: www.feynmanlectures.caltech.edu/flptapes.html\n'But each piece, or part of the whole of nature is only some kind of an approximation to the complete truth...'\n'Very small effects will have profound changes on our ideas.'\n'Even everything that we know is only some kind of approximation...'\n'But what is the source of the knowledge, where do the laws come from that are going to be tested?'\n'...to guess at the wonderful, simple, yet very strange patterns beneath it all...'";
 
 export default te;
