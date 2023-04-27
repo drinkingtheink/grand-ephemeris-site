bbf05db651410dc4255be1c298b6d5fd4590689d
drinkingtheink <jason.m.harrison@gmail.com>
2023-04-27 16:26:34 -0500

    lyrics

---
+++ src/data/mare.js
@@ -0,0 +1,5 @@
+/* eslint no-useless-escape: 0 */
+
+const mi = "\nYou in the market\nFor the brand new home of your dreams\nIt'll cost you everything\nBut the views are immaculate\nRecently lost your\nBodily autonomy\nReduced to less than nothing degrees\nCompletely irrelevant\nA new day dawns now\nAnd haven't I got just the thing\nBring only what you need to bring\nEscape velocity\nDon't take too long now\nBuyers lining up to sign the lease\nA time share in the astral peaks\nYour own relativity\nFind your continuum\nIn absentia\nWrite your own compendium\nAd infinitum\nOn Mare Imbrium"
+
+export default mi;
