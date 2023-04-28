f58d3d5c3a6aaede58559e1659343d020f9f55ef
drinkingtheink <jason.m.harrison@gmail.com>
2023-04-27 20:56:41 -0500

    lyrics

--- src/data/saint-juan.js
+++ src/data/saint-juan.js
@@ -1,5 +1,5 @@
 /* eslint no-useless-escape: 0 */
 
-const sj = "Audio samples taken from \"Welcome to Southern California\"(1953)\""
+const sj = "Audio samples taken from \"Welcome to Southern California (1953)\""
 
 export default sj;
